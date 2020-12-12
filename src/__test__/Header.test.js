f9c0359d7ae5fdfad062657378c152f0e3a572c9
ihollander <ianholla@gmail.com>
2020-12-12 18:13:18 -0500

    Updated tests and readme

---
+++ src/__test__/Header.test.js
@@ -0,0 +1,16 @@
+import "@testing-library/jest-dom";
+import { render, screen, fireEvent } from "@testing-library/react";
+import Header from "../components/Header";
+
+test("displays the toggle button", () => {
+  render(<Header />);
+  expect(screen.queryByText(/ Mode/)).toBeInTheDocument();
+});
+
+test("calls the onDarkModeClick callback prop when the button is clicked", () => {
+  const onDarkModeClick = jest.fn();
+  render(<Header onDarkModeClick={onDarkModeClick} />);
+
+  fireEvent.click(screen.queryByText(/ Mode/));
+  expect(onDarkModeClick).toHaveBeenCalled();
+});
